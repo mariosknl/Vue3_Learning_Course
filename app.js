3111b38a42a9e0df2c33ae81887b480defa24994
mariosknl <mariosknl@outlook.com>
2020-12-18 11:53:14 +0200

    outputting v-for

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
-      x: 0,
-      y: 0,
+      books: [
+        {
+          title: "name of the wind",
+          author: "Patrick Rothfuss",
+        },
+        {
+          title: "The way of the kings",
+          author: "Brandon Sanderson",
+        },
+        {
+          title: "The Final Empire",
+          author: "Brandon Sanderson",
+        },
+      ],
     };
   },
   methods: {
